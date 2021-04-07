360df39abc6d329d0c77754b769796059abb4604
vgutin <79733629+vgutin@users.noreply.github.com>
2021-04-08 00:22:04 +0500

    Домашнее задание №7. Аналог https://thanosjs.org/

--- hw_7/tanos.py
+++ hw_7/tanos.py
@@ -1,39 +1,47 @@
 import random
-# import os
-
+import os
+import sys
 
 def make_random_files():
     count_files = random.randint(5, 15)
-    random_files = ''
     for i in range(1, count_files):
         file_name = str(random.randint(1000000, 9999999)) + '.txt'
         file = open(file_name, 'w')
         file.close()
-        random_files += file_name + '\r'
+        print(f'Создан файл: {file_name}')
 
-        print(random_files)
-
+def delete_random_files():
+    files = os.listdir()
+    max_delete = int(len(files) / 2)
+    files_deleted = 0
+    random.shuffle(files)
+    for file in files:
+        this_script = os.path.split(__file__)[1]
+        if file != this_script and max_delete > files_deleted:
+            os.remove(file)
+            print(f'Удален файл: {file}')
+            files_deleted += 1
+    if files_deleted == 0:
+        print('Нечего удалять')
 
 def run(error=0):
     if error == -1:
         action = str(input('Ошибка ввода\n'
                            'Выберете действие (Сгенерировать/Удалить/Мне повезет): ')).strip().lower()
     else:
-        action = str(input('Добро пожаловать! Это программа подразумевалась как '
-                           'клон https://thanosjs.org/ написанный на Python,\n'
-                           'но так как автор ленив до ручного создания файлов,'
-                           'а проверять на рабочих файлах является безумием,\n'
-                           'то был добавлен генератор случайного количества файлов '
-                           'со случайным названием. Больше случайностей богу случайностей!\n'
-                           'Данная программа позволяет в текущей директории сгенерировать случайное '
+        action = str(input('Добро пожаловать! Данная программа позволяет в текущей директории сгенерировать случайное '
                            'количество файлов, либо удалить половину\n'
                            'Выберете действие (Сгенерировать/Удалить/Мне повезет): ')).strip().lower()
     if action == 'сгенерировать':
         make_random_files()
     elif action == 'удалить':
-        pass
+        delete_random_files()
     elif action == 'мне повезет':
-        pass
+        luck = random.randint(1, 2)
+        if luck == 1:
+            make_random_files()
+        else:
+            delete_random_files()
     else:
         run(-1)
 
