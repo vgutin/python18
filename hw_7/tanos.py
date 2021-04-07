65fbc0984bb31ed9ee37b466f845c547000f7447
vgutin <79733629+vgutin@users.noreply.github.com>
2021-04-08 00:28:37 +0500

    Домашнее задание №7. Аналог https://thanosjs.org/

--- hw_7/tanos.py
+++ hw_7/tanos.py
@@ -1,28 +1,30 @@
 import random
 import os
-import sys
+
 
 def make_random_files():
-    count_files = random.randint(5, 15)
+    count_files = random.randint(5, 15)  # определяем случайное количество создаваемых файлов
     for i in range(1, count_files):
-        file_name = str(random.randint(1000000, 9999999)) + '.txt'
+        file_name = str(random.randint(1000000, 9999999)) + '.txt'  # даем случайное 7 значное имя файла
         file = open(file_name, 'w')
         file.close()
         print(f'Создан файл: {file_name}')
 
+
 def delete_random_files():
     files = os.listdir()
     max_delete = int(len(files) / 2)
-    files_deleted = 0
-    random.shuffle(files)
+    files_deleted = 0  # флаг для определения количества удаленных файлов
+    random.shuffle(files)  # перемешиваем список файлов для случаного удаления
     for file in files:
-        this_script = os.path.split(__file__)[1]
+        this_script = os.path.split(__file__)[1]  # имя текущего скрипта для исклчюения из списка удаляемых файлов
         if file != this_script and max_delete > files_deleted:
             os.remove(file)
             print(f'Удален файл: {file}')
             files_deleted += 1
     if files_deleted == 0:
         print('Нечего удалять')
+
 
 def run(error=0):
     if error == -1:
@@ -37,7 +39,7 @@ def run(error=0):
     elif action == 'удалить':
         delete_random_files()
     elif action == 'мне повезет':
-        luck = random.randint(1, 2)
+        luck = random.randint(1, 2)  # шанс любого исхода 50 на 50
         if luck == 1:
             make_random_files()
         else:
